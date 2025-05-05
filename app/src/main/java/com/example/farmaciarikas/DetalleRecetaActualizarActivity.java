79e49574113232430b6215ee66081a20b5d64689
Ar-Adunakhor <mm22108@ues.edu.sv>
2025-05-05 17:35:15 -0600

    Vistas Detalle Receta Completas

--- app/src/main/java/com/example/farmaciarikas/DetalleRecetaActualizarActivity.java
+++ app/src/main/java/com/example/farmaciarikas/DetalleRecetaActualizarActivity.java
@@ -1,6 +1,8 @@
 package com.example.farmaciarikas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DetalleRecetaActualizarActivity extends AppCompatActivity {
 
+    Button buscar;
+    Boolean yaBusco = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detalle_receta_actualizar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buscar = findViewById(R.id.buttonActualizarDetalleReceta);
+        buscar.setText(R.string.ActualizarDetalleReceta_btn_buscar);
+
+        //Logica para cambiar de Buscar a Actualizar.
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!yaBusco){
+                    yaBusco=true;
+                    buscar.setText(R.string.ActualizarDetalleReceta_btn_actualizar);
+                }
+                else{
+                    yaBusco=false;
+                    buscar.setText(R.string.ActualizarDetalleReceta_btn_buscar);
+                }
+
+            }
         });
     }
 }
