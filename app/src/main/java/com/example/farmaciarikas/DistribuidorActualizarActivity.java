e6b508a9e94cb19e2db8945adb572b599a3959bc
Ar-Adunakhor <mm22108@ues.edu.sv>
2025-05-05 16:55:07 -0600

    Vistas del Distribuidor Completas

--- app/src/main/java/com/example/farmaciarikas/DistribuidorActualizarActivity.java
+++ app/src/main/java/com/example/farmaciarikas/DistribuidorActualizarActivity.java
@@ -1,6 +1,8 @@
 package com.example.farmaciarikas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DistribuidorActualizarActivity extends AppCompatActivity {
 
+    Button buscar;
+    Boolean yaBusco = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_distribuidor_actualizar);
+
+        buscar = findViewById(R.id.buttonActualizarDistribuidor);
+        buscar.setText(R.string.ActualizarDistribuidor_btn_buscar);
+
+        //Logica para cambiar de Buscar a Actualizar.
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!yaBusco){
+                    yaBusco=true;
+                    buscar.setText(R.string.ActualizarDistribuidor_btn_actualizar);
+                }
+                else{
+                    yaBusco=false;
+                    buscar.setText(R.string.ActualizarDistribuidor_btn_buscar);
+                }
+
+            }
+        });
     }
 }
