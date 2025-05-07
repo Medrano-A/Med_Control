98780864b507ab95545a821327d22f725710c268
JaimeGD09 <jaimegildiaz3838@gmail.com>
2025-05-06 19:53:03 -0600

    definicion de entidades gd21001

--- app/src/main/java/com/example/farmaciarikas/Laboratorio.java
+++ app/src/main/java/com/example/farmaciarikas/Laboratorio.java
@@ -1,4 +1,51 @@
 package com.example.farmaciarikas;
 
 public class Laboratorio {
+
+    private int idLaboratorio;
+    private String nombre;
+    private String tipo;
+    private String telefono;
+
+    public int getIdLaboratorio() {
+        return idLaboratorio;
+    }
+
+    public void setIdLaboratorio(int idLaboratorio) {
+        this.idLaboratorio = idLaboratorio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Laboratorio() {
+    }
+
+    public Laboratorio(int idLaboratorio, String nombre, String tipo, String telefono) {
+        this.idLaboratorio = idLaboratorio;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.telefono = telefono;
+    }
 }
--- app/src/main/java/com/example/farmaciarikas/Transaccion.java
+++ app/src/main/java/com/example/farmaciarikas/Transaccion.java
@@ -1,4 +1,83 @@
 package com.example.farmaciarikas;
 
+import java.time.LocalDate;
+
 public class Transaccion {
+
+    private int idVenta;
+    private String Dui;
+    private int IdUsuario;
+    private int id_local;
+    private LocalDate fechaEmision;
+    private double total;
+    private String tipo;
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public String getDui() {
+        return Dui;
+    }
+
+    public void setDui(String dui) {
+        Dui = dui;
+    }
+
+    public int getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        IdUsuario = idUsuario;
+    }
+
+    public int getId_local() {
+        return id_local;
+    }
+
+    public void setId_local(int id_local) {
+        this.id_local = id_local;
+    }
+
+    public LocalDate getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(LocalDate fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Transaccion() {
+    }
+
+    public Transaccion(int idVenta, String dui, int idUsuario, int id_local, LocalDate fechaEmision, double total, String tipo) {
+        this.idVenta = idVenta;
+        Dui = dui;
+        IdUsuario = idUsuario;
+        this.id_local = id_local;
+        this.fechaEmision = fechaEmision;
+        this.total = total;
+        this.tipo = tipo;
+    }
 }
